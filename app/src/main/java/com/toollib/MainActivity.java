561dcb425609542605d5628f6f736104a728361a
zhenglsT007 <mr.zheng@dookay.com>
2017-03-17 09:02:03 +0800

    更新方法

--- app/src/main/java/com/toollib/MainActivity.java
+++ app/src/main/java/com/toollib/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.toollibrary.viewutil.TostUtil.TostUtilView;
@@ -25,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
 
         myDialog = new AlertDialog.Builder(MainActivity.this).create();
 
+/*        myDialog.getWindow().setContentView(R.layout.dilogview);
+        WindowManager.LayoutParams params = myDialog.getWindow().getAttributes();
+        params.width = this.getWindowManager().getDefaultDisplay().getWidth();
+
+  *//*      params.height = 50;*//*
+        myDialog.getWindow().setAttributes(params);*/
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     myDialog.show();
                     myDialog.getWindow().setContentView(R.layout.dilogview);
+                    WindowManager.LayoutParams params = myDialog.getWindow().getAttributes();
+                    params.width = 200;
+                    params.alpha = 0.3f;
+  /*      params.height = 50;*/
+                    myDialog.getWindow().setAttributes(params);
+
                 }
 
 
