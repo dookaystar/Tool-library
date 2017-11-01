561fe7aa9b88a81058944f3cf2f9c4428ee086fa
zhenglsT007 <mr.zheng@dookay.com>
2017-11-01 08:39:52 +0800

    更新方法

--- app/src/main/java/com/toollib/MainActivity.java
+++ app/src/main/java/com/toollib/MainActivity.java
@@ -1,76 +1,51 @@
 package com.toollib;
 
-
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 
-import com.toollibrary.tool.*;
-import com.toollibrary.viewutil.dialogutil.CustomDialog;
-import com.zhy.http.okhttp.OkHttpUtils;
-import com.zhy.http.okhttp.callback.StringCallback;
-
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.Cache;
-import okhttp3.Call;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    boolean aBoolean;
-    Button button;
 
-    private static final long cacheSize = 1024 * 1024 * 20;//缓存文件最大限制大小20M
-    private static String cachedirectory = "" + "/caches";  //设置缓存文件路径
-    private static Cache cache = new Cache(new File(cachedirectory), cacheSize);  //
+
+    ImageView imageView;
+
+    boolean aBoolean;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
+        super.onCreate(savedInstanceState);/* setContentView(R.la);*/
+        setContentView(R.layout.show);
+
+        imageView = (ImageView) findViewById(R.id.imgshowview);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View v) {  //测试缓存
-
-                                          CustomDialog customDialog = new CustomDialog(MainActivity.this);
-                                          customDialog.show();
-                                    /*      OkHttpClient okHttpClient = new OkHttpClient();
-                                          OkHttpClient okHttpClient1 = okHttpClient.newBuilder().addInterceptor(new CacheInterceptor()).cache(new Cache(getCacheDir(), 10240 * 1024)).connectTimeout(20, TimeUnit.SECONDS)
-                                                  .readTimeout(20, TimeUnit.SECONDS)
-                                                  .build();*/
-                                       /*   OkHttpUtils okHttpClient2 = OkHttpUtils.initClient(okHttpClient1);
-                                          okHttpClient2.getOkHttpClient().newBuilder().addInterceptor();*/
-
-                                          //     OkHttpUtils.initClient(new OkHttpClient()).getOkHttpClient().newBuilder().addInterceptor(new CacheInterceptor()).cache(new Cache(getCacheDir(), 10240 * 1024)).connectTimeout(20, TimeUnit.SECONDS).build().newCall();
-
-                                          /*      OkHttpUtils.get().build().buildCall()*/
 
 
-                                    /*     OkHttpUtils.get().build().writeTimeOut(1000).getRequest().newBuilder().cacheControl(new CacheInterceptor())*/
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                                          //     OkHttpUtils.get().build().writeTimeOut(1000).getRequest().newBuilder().cacheControl(new Cache(getCacheDir(), 10240 * 1024)).c
+             /*   if (!aBoolean) {
+//selectableItemBackgroundBorderless
+                    imageView.setBackground(getDrawable(R.drawable.showbroud));
+                } else {
+                    imageView.setBackground(null);
+                }
+*/
 
-                                      }
-                                  }
+                aBoolean = !aBoolean;
 
-        );
+            }
+        });
+
+
     }
+/*
 
     public class CacheInterceptor implements Interceptor {
         @Override
@@ -86,5 +61,6 @@ public class MainActivity extends AppCompatActivity {
             return response1;
         }
     }
+*/
 
 }
